efc85d20eadcf0d96ba3d6c714f2966e5c03959e
saad <125143474+saadbrb@users.noreply.github.com>
2024-01-17 00:37:25 +0100

    Regestrierung erledigt

---
+++ app/src/main/java/com/example/funtasktic_app_/UserDatabaseHandler.java
@@ -0,0 +1,51 @@
+package com.example.funtasktic_app_;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDatabaseHandler extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "UserDatabase";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "users";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_PASSWORD = "password";
+
+    public UserDatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_USERNAME + " TEXT PRIMARY KEY, " + COLUMN_PASSWORD + " TEXT)";
+        db.execSQL(createTableStatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // DantenBank ändern, falls andere Verion vorhanden
+    }
+
+    public boolean addUser(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_USERNAME, username);
+        cv.put(COLUMN_PASSWORD, password);
+
+        long insert = db.insert(TABLE_NAME, null, cv);
+        return insert != -1;
+    }
+
+    public boolean checkUserExists(String username, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_USERNAME + " = ? AND " + COLUMN_PASSWORD + " = ?";
+        Cursor cursor = db.rawQuery(query, new String[]{username, password});
+
+        boolean exists = cursor.getCount() > 0;
+        cursor.close();
+        return exists;
+    }
+}
