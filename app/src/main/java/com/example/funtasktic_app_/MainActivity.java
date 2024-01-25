3bdf810fb1ec27e11c5172dd86f780790fef4db7
Shams <shams.kabir@stud.h-da.de>
2024-01-25 17:48:31 +0100

    icon-added

--- app/src/main/java/com/example/funtasktic_app_/MainActivity.java
+++ app/src/main/java/com/example/funtasktic_app_/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
